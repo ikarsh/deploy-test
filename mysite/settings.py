eb62fb5f36600df70942b045bd84e6e38d9dd845
me <me>
2025-06-08 13:39:20 +0300

    allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,6 +29,9 @@ SECRET_KEY = "django-insecure-fs$+im%dg*3xah=l^0#s@stq5+ysj1s^cazni)=crvlmo3btts
 DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
